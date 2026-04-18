733060c5c9444cc5bb5456324904d982bf83bf33
pengwen101 <111033605+pengwen101@users.noreply.github.com>
2026-04-18 14:03:26 +0700

    Updates

---
+++ assets/03_dog_breed_classification-Cajvn7ig.js
@@ -0,0 +1,8 @@
+var e=`# Dog Breed Classification\r
+\r
+![alt text](/img/dog_classification.png)\r
+\r
+An image classification model using transfer learning with ResNet50 as the backbone for classifying 120 dog breeds in the Stanford Dog Dataset. The model achieved satisfactory accuracy with ~90% accuracy in train set and ~80% accuracy in test set.\r
+\r
+Tags=Deep Learning,Computer Vision\r
+Github=https://github.com/pengwen101/dog-breed-classification`;export{e as default};
---
+++ assets/05_riasec_search_gap-CPdgbN5Z.js
@@ -0,0 +1,8 @@
+var e=`# Search Jobs & Analyze Skill Gaps Based on RIASEC Traits\r
+\r
+![alt text](/img/riasec_gap_analysis.png)\r
+\r
+A web application utilizing using multi-agent LLM to recommend job vacancies sourced from Petra's alumni website and analyze the gaps between a user's RIASEC traits and a job's description or requirements. It further suggests educational videos to help bridge those gaps.\r
+\r
+Tags=NLP\r
+Github=https://github.com/pengwen101/riasec-assessment/tree/2.0`;export{e as default};
