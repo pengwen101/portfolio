9616a5a8950247c7016bcafd78deb5bbb9278f0e
pengwen101 <111033605+pengwen101@users.noreply.github.com>
2026-04-18 14:04:25 +0700

    Updates

---
+++ assets/01_sumatra_topic_model-B5qii7uM.js
@@ -0,0 +1,9 @@
+var e=`# Sumatra Flood 2025: Public Perceptions of the Indonesian Government’s Disaster Management\r
+\r
+![alt text](/img/sumatra_topic_model.png)\r
+\r
+This undergraduate research project, supervised by a university professor, explores the intersection of digital technology and sustainability. I analyzed over 30,000 YouTube comments regarding a 2025 natural disaster in Sumatra. Through this analysis, I identified patterns and sentiment polarity among state actors, as well as a high correlation between government and environmental topics (further details can be found in the paper). Topic modeling was conducted using BERTopic, and I utilized statistical methods such as Chi-squared tests and Pearson correlation.\r
+\r
+Tags=NLP, Web Scraping, Research\r
+Paper=https://drive.google.com/file/u/1/d/1cuh2BrRHiRKkZu3bLZBv39QWdO4wrjXA/view?usp=sharing\r
+Github=https://github.com/pengwen101/2025-sumatra-flood-analysis`;export{e as default};
